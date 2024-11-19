682654d9623b15924a303e3ad7ed96a0c30aa694
Ashraf <ashraf.git.com>
2024-11-19 10:11:02 +0530

    Dcesign is added

---
+++ Design.java
@@ -0,0 +1,8 @@
+public class Design {
+    public static void main(String[] args) {
+        System.out.println("HI......");
+        System.out.println("HI......");
+        System.out.println("HI......");
+        System.out.println("HI......");
+    }
+}
